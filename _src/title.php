2761a4879d802c0c56706876b72f462e4dd90c0c
kkeundotnet <kkeundotnet@gmail.com>
2017-05-07 22:57:27 +0900

    Sub title

--- _src/title.php
+++ _src/title.php
@@ -4,6 +4,7 @@ namespace title;
 function echoo()
 { ?>
 <p>
-<a href="<?php echo BASE_URL; ?>"><?php echo BLOG_TITLE; ?></a>
+<a href="<?php echo BASE_URL; ?>"><?php echo BLOG_TITLE; ?></a> |
+<?php echo BLOG_SUB_TITLE; ?>
 </p>
 <?php }
