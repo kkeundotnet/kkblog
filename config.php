19a2db22520dfa98f31d6eeeec025b89c5bf9574
kkeundotnet <kkeundotnet@gmail.com>
2017-05-08 00:31:58 +0900

    Rename software

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 define('BLOG_TITLE', "kkblog");
-define('BLOG_DESC', "ㄲ 블로그 엔진");
+define('BLOG_DESC', "ㄲ블로그");
 define('BASE_URL', "https://blog.kkeun.net/");
 define('ENABLE_DISQUS', true);
 define('DISQUS_ID', "kkeundotnet");
