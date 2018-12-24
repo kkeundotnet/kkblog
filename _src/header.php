eceec60ab090476e9952fb939ec7419fdc42631b
kkeundotnet <kkeundotnet@gmail.com>
2018-12-25 04:09:30 +0900

    Upgrade showdown 1.9.0

--- _src/header.php
+++ _src/header.php
@@ -16,7 +16,7 @@ if(!is_null($t_sub))
 <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL_S.'css/kkblog.css'; ?>">
 <?php if (ENABLE_KKOMENT): ?>
 <script src='https://cdn.rawgit.com/jackmoore/autosize/4.0.2/dist/autosize.min.js'></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/showdown/1.8.6/showdown.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/showdown/1.9.0/showdown.min.js'></script>
 <script src="https://kkoment.kkeun.net/kkoment.js" charset="UTF-8"></script>
 <?php endif; ?>
 <title><?php echo $t; ?></title>
